d372bcb41fd2553c70db72d54f96aa2236123db4
NBurek <NickBurek@gmail.com>
2012-04-22 16:32:54 -0500

    Working on the WidgetShield library.

--- WidgetShield.cpp
+++ WidgetShield.cpp
@@ -1,32 +1,75 @@
 /**
  *	To create a reference to this singleton class use the following:
- *		WidgetCore* wC = WidgetCore::Instance();
+ *		WidgetShield* wC = WidgetShield::Instance();
  */
-#include "WidgetCore.h"
+#include "WidgetShield.h"
 
-WidgetCore* WidgetCore::p_instance = NULL;
+WidgetShield* WidgetShield::p_instance = NULL;
 
-WidgetCore* WidgetCore::Instance()
+WidgetShield* WidgetShield::Instance()
 {
-	if (p_instance==NULL) p_instance= new WidgetCore;
+	if (p_instance==NULL) p_instance= new WidgetShield;
 	return p_instance;
 }
 
-WidgetCore::WidgetCore()
+WidgetShield::WidgetShield()
 {
-	
+	Serial.begin(9600);
 }
 
-void WidgetCore::addWidget(Widget w)
+void WidgetShield::addWidget(Widget w)
 {
-	
+	char data[1];
+	data[0] = w.getType();
+	sendData(ADD_WIDGET_COMMAND,data,0);
+	w.initGraphics();
 }
-void WidgetCore::removeWidget(Widget w)
+void WidgetShield::removeWidget(Widget w)
 {
-	
+	char data[1];
+	data[0] = w.getID();
+	sendData(REMOVE_WIDGET_COMMAND,data,0);
 }
 
-void WidgetCore::sendData(char data[],int length)
+void WidgetShield::moveWidget(Widget w, uint16_t x, uint16_t y)
+{
+	char data[5];
+	
+	//get the widget ID here and put it in data[0]
+	//data[0] = w.
+	uint16ToCharArray(x,data+1);
+	uint16ToCharArray(y,data+3);
+	
+	sendData(MOVE_WIDGET_COMMAND,data,5);
+}
+
+void WidgetShield::sendWidgetCommand(char data[], char length)
+{
+	sendData(SEND_WIDGET_COMMAND,data,length);
+}
+
+void WidgetShield::sendData(char function, char data[],char length)
 {
 	//need to first send the start command and length
+	Serial.print(START_SEND_COMMAND);
+	if (function == START_SEND_COMMAND || function == ESCAPE_CHARACTER)
+		Serial.print(ESCAPE_CHARACTER);
+	Serial.print(function);
+	if (length == START_SEND_COMMAND || length == ESCAPE_CHARACTER)
+		Serial.print(ESCAPE_CHARACTER);
+	Serial.print(length);
+	
+	for (char i = 0; i<length; ++char)
+	{
+		if (data[i] == START_SEND_COMMAND || data[i]==ESCAPE_CHARACTER)
+			Serial.print(ESCAPE_CHARACTER);
+		Serial.print(data[i]);
+	}
+}
+
+
+void WidgetShield::uint16ToCharArray(uint16_t i, char* data)
+{
+	data[0] = (char) (i >> 8);
+	data[1] = (char) (i & 0x0F);
 }
--- WidgetShield.h
+++ WidgetShield.h
@@ -1,18 +1,29 @@
-#ifndef WidgetCore_h
-#define WidgetCore_h
+#ifndef WidgetShield_h
+#define WidgetShield_h
 
 #include "WProgram.h"
 
-class WidgetCore
+#define START_SEND_COMMAND 0x7C
+#define ESCAPE_CHARACTER 0x7D
+#define ADD_WIDGET_COMMAND 0x01
+#define REMOVE_WIDGET_COMMAND 0x02
+#define MOVE_WIDGET_COMMAND 0x03
+#define SEND_WIDGET_COMMAND 0x04 
+
+
+class WidgetShield
 {
 	public:
-		WidgetCore();
+		WidgetShield();
+		static WidgetShield* Instance();
 		void addWidget(Widget w);
 		void removeWidget(Widget w);
-		static WidgetCore* Instance();
+		void moveWidget(Widget w, uint16_t x, uint16_t y);
+		void sendWidgetCommand(char data[], char length)
 	private:
-		static WidgetCore *p_instance;
-		void sendData(char data[],int length);
+		static WidgetShield *p_instance;
+		void sendData(char function, char data[],char length);
+		void uint16ToCharArray(uint16_t i, char* data);
 
 }
 
